9e91dad05b1a737e2d185a61e0f5bf5988fcc95e
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-09-30 09:45:23 +0200

    properties made to bind to

--- WpfImageClassification/ViewModel/StatisticsViewModel.cs
+++ WpfImageClassification/ViewModel/StatisticsViewModel.cs
@@ -13,10 +13,25 @@ namespace WpfImageClassification.ViewModel
     {
         private ICommand _goToMenuViewCommand;
         private readonly Action _goToMenuViewAction;
-        private double _accuracy = 0.95;
-        private double _precision;
-        private double _recall;
-        private double _f1Score;
+        private double _cardboardAccuracy = 0.95;
+        private double _cardboardPrecision;
+        private double _cardboardRecall;
+        private double _cardboardF1Score;
+
+        private double _plasticAccuracy;
+        private double _plasticPrecision;
+        private double _plasticRecall;
+        private double _plasticF1Score;
+
+        private double _metalAccuracy;
+        private double _metalPrecision;
+        private double _metalRecall;
+        private double _metalF1Score;
+
+        private double _glassAccuracy;
+        private double _glassPrecision;
+        private double _glassRecall;
+        private double _glassF1Score;
 
         private string _statisticsFilePath = "C:\\modelstatistics.txt";
 
@@ -24,19 +39,123 @@ namespace WpfImageClassification.ViewModel
         {
             get => _goToMenuViewCommand ??= new BaseCommand(GoToMenuView);
         }
-        public double Accuracy
+        public double CardboardAccuracy
         {
-            get => _accuracy;
+            get => _cardboardAccuracy;
             set
             {
-                _accuracy = value;
+                _cardboardAccuracy = value;
                 OnPropertyChanged();
             }
         }
-        public double Precision { get => _precision; set => _precision = value; }
-        public double Recall { get => _recall; set => _recall = value; }
-        public double F1Score { get => _f1Score; set => _f1Score = value; }
-
+        public double CardboardPrecision 
+        { 
+            get => _cardboardPrecision;
+            set
+            {
+                _cardboardPrecision = value;
+                OnPropertyChanged();
+            }
+        }
+        public double CardboardRecall 
+        { 
+            get => _cardboardRecall;
+            set
+            {
+                _cardboardRecall = value;
+                OnPropertyChanged();
+            }
+        }
+        public double CardboardF1Score
+        {
+            get => _cardboardF1Score;
+            set
+            {
+                _cardboardF1Score = value;
+                OnPropertyChanged();
+            }
+        }
+        public double GlassPrecision 
+        { 
+            get => _glassPrecision;
+            set
+            {
+                _glassPrecision = value;
+                OnPropertyChanged();
+            }
+        }
+        public double GlassRecall 
+        { 
+            get => _glassRecall;
+            set
+            {
+                _glassRecall = value;
+                OnPropertyChanged();
+            }
+        }
+        public double GlassF1Score 
+        { 
+            get => _glassF1Score;
+            set
+            {
+                _glassF1Score = value;
+                OnPropertyChanged();
+            }
+        }
+        public double MetalPrecision 
+        { 
+            get => _metalPrecision;
+            set
+            {
+                _metalPrecision = value;
+                OnPropertyChanged();
+            }
+        }
+        public double MetalRecall 
+        { 
+            get => _metalRecall;
+            set
+            {
+                _metalRecall = value;
+                OnPropertyChanged();
+            }
+        }
+        public double MetalF1Score 
+        { 
+            get => _metalF1Score;
+            set
+            {
+                _metalF1Score = value;
+                OnPropertyChanged();
+            }
+        }
+        public double PlasticPrecision 
+        { 
+            get => _plasticPrecision;
+            set
+            {
+                _plasticPrecision = value;
+                OnPropertyChanged();
+            }
+        }
+        public double PlasticRecall 
+        { 
+            get => _plasticRecall;
+            set
+            {
+                _plasticRecall = value;
+                OnPropertyChanged();
+            }
+        }
+        public double PlasticF1Score 
+        { 
+            get => _plasticF1Score;
+            set
+            {
+                _plasticF1Score = value;
+                OnPropertyChanged();
+            }
+        }
         public StatisticsViewModel(Action action)
         {
             _goToMenuViewAction = action;
